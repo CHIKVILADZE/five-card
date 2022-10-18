2a25259768a45d5a4ef22cddc6c7a2cecb0fc16d
George <103374242+CHIKVILADZE@users.noreply.github.com>
2022-10-18 16:34:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const panels = document.querySelectorAll (".panel");
+
+
+panels.forEach(panel => {
+    panel.addEventListener ('click', () => {
+        removeActivClasses ()
+        panel.classList.add('active')
+        
+        
+       
+    })
+})
+
+
+
+
+
+
+
+
+function removeActivClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+
